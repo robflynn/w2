befc2499feaf67512ed222f4fb549ede78237bc3
Rob Flynn <rob@thingerly.com>
2018-12-02 17:39:52 -0500

    Add promises

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "webster",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/freshOS/then.git", from: "4.2.0")        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "webster",
-            dependencies: []),
+            dependencies: [
+                "then"
+            ]),
         .testTarget(
             name: "websterTests",
             dependencies: ["webster"]),
--- Sources/webster/main.swift
+++ Sources/webster/main.swift
@@ -1,1 +1,30 @@
-print("Hello, world!")
+import then
+
+protocol API {
+
+}
+
+extension API {
+
+}
+
+struct Website {
+    var name: String
+}
+
+class Client: API {
+    func websites() -> Promise<[Website]> {
+        return Promise<[Website]> { resolve, reject in 
+            resolve([Website(name: "Google"), Website(name: "Apple")])
+        }        
+    }
+}
+
+// get list of websites
+let api = Client()
+
+let websites = try! await(api.websites())
+
+for website in websites {
+    print(website.name)
+}
