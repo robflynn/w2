093570c9bbf020a0f79cb47f05fba06e35dcbb66
Rob Flynn <rob@thingerly.com>
2018-12-04 23:59:04 -0500

    commit before bed - added notes, amend

--- Sources/webster/main.swift
+++ Sources/webster/main.swift
@@ -8,8 +8,12 @@ public final class Logger {
     }
 }
 
+// NOTE: Do I wanna just move this all into a class? 
+let DEFAULT_PAGES_PER_SECOND: TimeInterval = 1.0
+
 let crawlingCount = 0
 let logger = Logger.self
+let pageLoaderQueue = DispatchQueue(label: "com.thingerly.webster.pageloaderque")
 
 func getWebsites() -> [Website] {
     guard let websites = try? await(api.websites()) else {
@@ -81,7 +85,7 @@ func sendPageToServer(response: PageResponse) -> Promise<Page> {
     }
 }
 
-func crawlLoop(website: Website) {
+func crawlLoop555(website: Website, rate: TimeInterval) {
     let pageGroup = DispatchGroup()
     let pageDispatchQueue = DispatchQueue(label: "websterPageQueue", attributes: .concurrent)
 
@@ -111,12 +115,74 @@ func crawlLoop(website: Website) {
         DispatchQueue.main.async {
             logger.debug("Finished with crawl queue. Looping.")
 
-            crawlLoop(website: website)
+            crawlLoop(website: website, rate: rate)
         }
     }
 }
 
-func crawlWebsite(named name: String) {
+func crawlLoop(website: Website, rate: TimeInterval) {
+    // Get the next batch of pages
+    let pageBatch = getPageQueue(for: website)
+}
+
+class Webster {
+    typealias Rate = TimeInterval
+
+    var rate: Rate = DEFAULT_PAGES_PER_SECOND
+
+    private var website: Website    
+
+    init(website: Website) {
+        self.website = website
+    }
+
+    convenience init(website: Website, rate: Rate) {
+        self.init(website: website)
+        self.rate = rate
+    }
+
+    static func crawl(website: Website, completion: (() -> Void)? = nil) {
+        self.crawl(website: website, rate: DEFAULT_PAGES_PER_SECOND, completion: completion)
+    }
+
+    static func crawl(website: Website, rate: Rate, completion: (() -> Void)? = nil) {
+        let spiderling = Webster(website: website, rate: rate)
+
+        spiderling.crawl {
+            logger.debug("Finished crawling, static method")
+
+            completion?()            
+        }
+    }
+
+
+    func crawl(completion: (() -> Void)? = nil) {
+        // Get a batch of pages
+        let pageBatch = getPageQueue(for: website)
+        let pageLoaderQueue = DispatchQueue(label: "com.thingerly.webster.pageloaderque")
+        var delay: TimeInterval
+
+        logger.debug("received page batch (\(pageBatch.pages.count)):")
+
+        if pageBatch.pages.isEmpty {
+            print("No pages to crawl.")
+            
+            completion?()
+
+            return
+        }
+
+        delay = 1 / self.rate
+        var currentDelay = 0
+        var startTime = Date()
+
+        // build out the queue throttler tomorrow, just do a synchronous 
+        // queue, throttled to pages per second, and then allow for
+        // multiple queues concurrently
+    }
+}
+
+func crawlWebsite(named name: String, pagesPerSecond rate: TimeInterval = DEFAULT_PAGES_PER_SECOND) {
     let websites = getWebsites()
     
     guard let website = websites.first(where: { $0.name == name }) else {
@@ -127,10 +193,9 @@ func crawlWebsite(named name: String) {
 
     print("Crawling \(website.url) ...")
 
-    crawlLoop(website: website)
-
-    // Fire up the main event loop, we're gonna be here a while
-    dispatchMain()    
+    Webster.crawl(website: website) {
+        logger.debug("Finished calling, command-spawned")
+    }
 }
 
 // get list of websites
@@ -150,8 +215,10 @@ Group {
         }
 
     $0.command("crawl", 
-            Argument<String>("name", description: "The name of the website to crawl")
-        ) { (name: String) in 
-            crawlWebsite(named: name)
+            Argument<String>("name", description: "The name of the website to crawl"),
+            Option<TimeInterval>("rate", default: DEFAULT_PAGES_PER_SECOND, description: "Max page loads per second"),
+            Option<Int>("batch-size", default: 5, description: "Number of pages to claim in a single batch")
+        ) { (name: String, rate: TimeInterval, batch: Int) in 
+            crawlWebsite(named: name, pagesPerSecond: rate)
         }
 }.run()
